4fb23e1cd80e2e1581116a7d1804b586da629f99
kaihengann <annkaiheng@gmail.com>
2019-07-03 15:24:20 +0800

    Add new route handlers, uuid and jwt library

--- src/models/userBlog.model.js
+++ src/models/userBlog.model.js
@@ -1,23 +1,22 @@
 const mongoose = require("mongoose");
-mongoose.set('useCreateIndex', true);
-const Schema = mongoose.Schema
-
-const postSchema = new Schema({
-  title: { type: String },
-  body: { type: String },
-  createOn: { type: Date, default: Date.now },
-  updatedOn: { type: Date, default: Date.now }
-});
+mongoose.set("useCreateIndex", true);
+const Schema = mongoose.Schema;
 
 const userBlogSchema = new Schema({
   username: { type: String, required: true, unique: true },
   password: {
     type: String,
-    required: true,
-    match: /(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,}/
+    required: true
   },
-  posts: [{ postSchema }],
-  salt: String
+  posts: [
+    {
+      postId: { type: String },
+      postTitle: { type: String },
+      postBody: { type: String },
+      createdOn: { type: Date, default: Date.now },
+      updatedOn: { type: Date, default: Date.now }
+    }
+  ]
 });
 
 module.exports = mongoose.model("userBlog", userBlogSchema);
