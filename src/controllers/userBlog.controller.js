b3b89e18a6e062d228033bccdc8c8ab1d1c128d1
kaihengann <annkaiheng@gmail.com>
2019-07-03 15:28:45 +0800

    Add new routes

--- src/controllers/userBlog.controller.js
+++ src/controllers/userBlog.controller.js
@@ -1,36 +1,211 @@
 const mongoose = require("mongoose");
 const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+const uuidv4 = require("uuid/v4");
 require("../utils/db");
 require("../models/userBlog.model");
 
+const generateToken = user =>
+  jwt.sign(
+    { sub: user._id, iat: new Date().getTime(), user: user.username },
+    "mysecret",
+    { expiresIn: "1h" }
+  );
+
+// Remove: Test data
 const UserBlogModel = mongoose.model("userBlog");
+const testUserBlog = new UserBlogModel({
+  username: uuidv4(),
+  password: "testPassword123",
+  posts: [
+    {
+      postId: uuidv4(),
+      postTitle: "myfirstpost",
+      postBody: "somecontentityped"
+    }
+  ]
+});
+testUserBlog.save();
 
-const findAllUsers = async (req, res) => {
-  // const UserBlogs = await db.getCollection("test");
-  const testSchema = mongoose.model('test', new Schema({ name: String }))
-  const foundUsers = await testSchema.find();
-  res.json(foundUsers);
+const findAllUserBlogs = async (req, res) => {
+  try {
+    const foundUserBlogs = await UserBlogModel.find();
+    res.json(foundUserBlogs);
+  } catch (err) {
+    const error = new Error("UserBlogs not found");
+    res.status(404).json(error.message);
+  }
 };
 
-
-const userSignUp = async input => {
-  const { username, password } = input;
-  const saltRound = 10;
-  const hash = await bcrypt.hash(password, saltRound);
-  userWithHash = {
-    username: username,
-    password: hash
-  };
-  const newUserBlog = new UserBlogModel(userWithHash);
-  return await newUserBlog.save();
+const findOneUserBlog = async (req, res) => {
+  try {
+    const { username } = req.params;
+    const foundUserBlog = await UserBlogModel.findOne({ username });
+    res.json(foundUserBlog);
+  } catch (err) {
+    const error = new Error("UserBlog not found");
+    res.status(404).json(error.message);
+  }
 };
 
-const userLogin = async input => {
-  const { username, password } = input;
-  const foundUser = await UserBlogModel.findOne({ username });
-  const isUser = await bcrypt.compare(password, foundUser.password);
-  if (isUser) return { username };
-  else return isUser;
+const findAllPosts = async (req, res) => {
+  try {
+    const { username } = req.params;
+    const foundUserBlog = await UserBlogModel.findOne({ username });
+    return res.json(foundUserBlog.posts);
+  } catch (err) {
+    const error = new Error("Posts not found");
+    res.status(404).json(error.message);
+  }
 };
 
-module.exports = { userSignUp, userLogin, findAllUsers };
+const findOnePost = async (req, res) => {
+  try {
+    const { username, postId } = req.query;
+    const foundUserBlog = await UserBlogModel.findOne({ username });
+    const posts = foundUserBlog.posts;
+    const foundPost = posts.find(post => post.postId === postId);
+    return res.json(foundPost);
+  } catch (err) {
+    const error = new Error("Post not found");
+    res.status(404).json(error.message);
+  }
+};
+
+const isUserLoggedIn = async (req, res, next) => {
+  try {
+    const header = req.headers.authorization;
+    const token = header.split(" ")[1];
+    const verifyToken = token => jwt.verify(token, "mysecret");
+    const _id = verifyToken(token).sub;
+    const foundUser = db.findOne({ _id });
+    if (foundUser) {
+      return res.json({ username: foundUser.username });
+    }
+    res.sendStatus(401);
+  } catch (err) {
+    next(err);
+  }
+};
+
+const createUserBlog = async (req, res) => {
+  try {
+    const { username, password } = req.body;
+    const saltRound = 10;
+    const hash = await bcrypt.hash(password, saltRound);
+    const userInfo = {
+      username,
+      password: hash
+    };
+    const newUserBlog = new UserBlogModel(userInfo);
+    await newUserBlog.save();
+    return res.status(201).json(newUserBlog);
+  } catch (err) {
+    const error = new Error("UserBlog not created");
+    res.status(400).json(error.message);
+  }
+};
+
+const createPost = async (req, res, next) => {
+  try {
+    const { username } = req.params;
+    const { postTitle, postBody } = req.body;
+    const newPost = {
+      postId: uuidv4(),
+      postTitle,
+      postBody,
+      createdOn: Date.now(),
+      updatedOn: Date.now()
+    };
+    const foundUserBlog = await UserBlogModel.findOne({ username });
+    const posts = foundUserBlog.posts;
+    posts.push(newPost);
+    await foundUserBlog.save();
+    return await res.status(201).json(newPost);
+  } catch (err) {
+    const error = new Error("Post not created");
+    res.status(400).json(error.message);
+  }
+};
+
+const updatePost = async (req, res, next) => {
+  try {
+    const { username, postId, postTitle, postBody, createdOn } = req.body;
+    const updatedPost = {
+      postId,
+      postTitle,
+      postBody,
+      createdOn,
+      updatedOn: Date.now()
+    };
+    const foundUserBlog = await UserBlogModel.findOne({ username });
+    const posts = foundUserBlog.posts;
+    const postIndex = posts.findIndex(post => post.postId === postId);
+    posts.splice(postIndex, 1, updatedPost);
+
+    await foundUserBlog.save();
+    res.json(updatedPost);
+  } catch (err) {
+    const error = new Error("Post not updated");
+    res.status(400).json(error.message);
+  }
+};
+
+const deletePost = async (req, res, next) => {
+  try {
+    const { username } = req.params;
+    const { postId } = req.body;
+    const foundUserBlog = await UserBlogModel.findOne({ username });
+    const posts = foundUserBlog.posts;
+    const postIndex = posts.findIndex(post => post.postId === postId);
+    posts.splice(postIndex, 1);
+    await foundUserBlog.save();
+    return await res.json(`Deleted post with postId:${postId} `);
+  } catch (err) {
+    const error = new Error("Post not deleted");
+    res.status(400).json(error.message);
+  }
+};
+
+const userLogin = async (req, res, next) => {
+  try {
+    const { username, password } = req.body;
+    const foundUser = await UserBlogModel.findOne({ username });
+    const isUser = await bcrypt.compare(password, foundUser.password);
+    if (isUser) {
+      const jwt = generateToken(foundUser);
+      return res.json({
+        username,
+        jwt
+      });
+    }
+    return res.status(401).json("Invalid username/password");
+  } catch (err) {
+    next(err);
+  }
+};
+
+// TODO: Add logout logic
+const userLogout = async (req, res, next) => {
+  try {
+    const { username } = req.body;
+    return res.status(200).json(`user: ${username} is logged out`);
+  } catch (err) {
+    const error = new Error("User not logged out");
+    res.status(401).json(error.message);
+  }
+};
+
+module.exports = {
+  userLogin,
+  userLogout,
+  isUserLoggedIn,
+  findAllUserBlogs,
+  findOneUserBlog,
+  findAllPosts,
+  findOnePost,
+  createUserBlog,
+  createPost,
+  deletePost,
+  updatePost
+};
