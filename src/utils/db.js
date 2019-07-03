abce938adcd4b457378810f6efeb6c041bd90190
kaihengann <annkaiheng@gmail.com>
2019-07-03 15:26:49 +0800

    Add currentEnv in console log

--- src/utils/db.js
+++ src/utils/db.js
@@ -1,16 +1,15 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const mongoOptions = {
-  useNewUrlParser: true,
+  useNewUrlParser: true
 };
 
-const dbUrl = process.env.MONGODB_URI || 'mongodb://localhost:27017/unblog';
+const dbUrl = process.env.MONGODB_URI || "mongodb://localhost:27017/unblog";
 mongoose.connect(dbUrl, mongoOptions);
 const db = mongoose.connection;
 
 const currentEnv = process.env.NODE_ENV || "development";
-db.on('error', console.error.bind(console, 'connection error:'));
-db.once('open', () => {
+db.on("error", console.error.bind(console, "connection error:"));
+db.once("open", () => {
   console.log(`connected to ${currentEnv} mongodb `);
 });
-
