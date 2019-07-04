3ca67a469576c410c283a3970efe565e63bd4232
kaihengann <annkaiheng@gmail.com>
2019-07-04 11:09:33 +0800

    Remove posts for user2 for testing

--- data/userBlogsData.js
+++ data/userBlogsData.js
@@ -17,14 +17,6 @@ module.exports = [
     _id: "5d1c74ae7b3b41f2d46f2d43",
     username: "user2",
     password: "Password2",
-    posts: [
-      {
-        postId: "postId2",
-        postTitle: "Post2",
-        postBody: "PostBody2",
-        createdOn: "2019-07-04T12:47:40.407Z",
-        updatedOn: "2019-07-04T12:47:40.407Z"
-      }
-    ]
+    posts: []
   }
 ];
