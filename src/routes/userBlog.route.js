8d2954664aa456e9a4bf69eea49d9c1082f06038
kaihengann <annkaiheng@gmail.com>
2019-07-06 16:23:39 +0800

    change authorization spelling

--- src/routes/userBlog.route.js
+++ src/routes/userBlog.route.js
@@ -1,20 +1,20 @@
 const { Router } = require("express");
 const Ctrl = require("../controllers/userBlog.controller");
-const authorisation = require("../middleware/authorisation")
+const authorization = require("../middleware/authorization")
 
 const userBlogRouter = Router();
 
 userBlogRouter.get("/", Ctrl.findAllUserBlogs);
 userBlogRouter.get("/users/:username", Ctrl.findOneUserBlog);
-userBlogRouter.get("/posts/:username", authorisation, Ctrl.findAllPosts);
-userBlogRouter.get("/posts", authorisation, Ctrl.findOnePost);
+userBlogRouter.get("/posts/:username", authorization, Ctrl.findAllPosts);
+userBlogRouter.get("/posts", authorization, Ctrl.findOnePost);
 userBlogRouter.post("/createUserBlog", Ctrl.createUserBlog);
-userBlogRouter.post("/posts/:username", authorisation, Ctrl.createPost);
-userBlogRouter.put("/posts/:username", authorisation, Ctrl.updatePost);
-userBlogRouter.delete("/posts/:username", authorisation, Ctrl.deletePost);
+userBlogRouter.post("/posts/:username", authorization, Ctrl.createPost);
+userBlogRouter.put("/posts/:username", authorization, Ctrl.updatePost);
+userBlogRouter.delete("/posts/:username", authorization, Ctrl.deletePost);
 
 userBlogRouter.post("/login", Ctrl.userLogin);
 userBlogRouter.post("/logout", Ctrl.userLogout);
-userBlogRouter.get("/secure/:username", authorisation, Ctrl.userLoggedIn);
+userBlogRouter.get("/secure/:username", Ctrl.userLoggedIn);
 
 module.exports = userBlogRouter;
