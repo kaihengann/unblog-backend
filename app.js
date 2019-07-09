5ba96e66d15f2b179dab68b4dfe5a06b173f7153
kaihengann <annkaiheng@gmail.com>
2019-07-09 09:56:58 +0800

    Add response for root directory

--- app.js
+++ app.js
@@ -2,10 +2,11 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 const userBlogRouter = require("./src/routes/userBlog.route");
-
+// TODO: whitelist origins
 app.use(cors());
 app.use(express.json());
 
+app.get("/", (req, res) => res.send("UnBlog API"));
 app.use("/userBlogs", userBlogRouter);
 
 app.use((err, req, res, next) => {
